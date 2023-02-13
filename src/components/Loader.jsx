06dcae85f97faab59c4d7bafb85bfb69feee3a08
samuelberhane <1samibrhane@gmail.com>
2023-02-13 13:39:37 +0300

    Delete Loader component folder.

---
+++ src/components/Loader.jsx
@@ -0,0 +1,11 @@
+import loader from "../../assets/loader.gif";
+
+const Loader = () => {
+  return (
+    <div className="bg-[rgba(0,0,0,0.2)] w-full h-[calc(100vh-100px)] fixed flex justify-center items-center top-0 bottom-0 left-0 right-0 z-50">
+      <img src={loader} alt="loader" className="w-[140px] rounded" />
+    </div>
+  );
+};
+
+export default Loader;

8b374a25e14d05620dad30fdcd710a64382e13d8
samuelberhane <1samibrhane@gmail.com>
2023-02-13 13:26:37 +0300

    Style Categories component using tailwind css.

---
+++ src/components/Categories.jsx
@@ -0,0 +1,31 @@
+import { Title } from ".";
+import "./categories.scss";
+import { data } from "../categoriesData";
+
+const Categories = () => {
+  return (
+    <div className="mb-8 px-4">
+      <Title title="Categories" />
+      <div className="mt-2 grid grid-cols-4 gap-4">
+        {data.map((category) => {
+          const { name, image, id } = category;
+          return (
+            <div className={`relative c${id}`}>
+              <img
+                src={image}
+                alt=""
+                key={id}
+                className="w-full h-[320px] rounded"
+              />
+              <p className="absolute top-4 left-4 bg-green-400 py-2 px-4 text-xl rounded text-white hover:bg-purple-600">
+                {name}
+              </p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Categories;

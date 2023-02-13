3a9c307c1d74f49fce0f159a36d22270d78812d6
samuelberhane <1samibrhane@gmail.com>
2023-02-13 13:28:02 +0300

    Style CategoryProducts component with tailwind css.

--- src/components/categoryProducts/CategoryProducts.jsx
+++ src/components/categoryProducts/CategoryProducts.jsx
@@ -7,15 +7,16 @@ import { ProductCard } from "../";
 const CategoryProducts = () => {
   return (
     <>
-      <div className="top">
+      <div className="mt-4">
         <img
           src="https://images.pexels.com/photos/3839432/pexels-photo-3839432.jpeg?auto=compress&cs=tinysrgb&w=1600"
           alt="cover"
+          className="w-full h-[350px] rounded-md"
         />
       </div>
-      <div className="bottom">
+      <div>
         <Title title="Men's" />
-        <div className="products">
+        <div>
           {data.map((product) => (
             <ProductCard key={product.id} product={product} />
           ))}
